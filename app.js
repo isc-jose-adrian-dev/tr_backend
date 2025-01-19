6068271c4aebf790b96a44211b5ddb764bd8ae7c
Jose Adrian <jose.adrian@cevisur.com.mx>
2025-01-19 09:11:09 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose=require("mongoose")
+app.use(express.json())
+const cors = require("cors");
+
+app.use(cors());
+const bcrypt = require('bcryptjs');
+app.use(bodyParser.json());
+
+const mongoUrl = "mongodb+srv://ingsantana36:HoHXlhVkV7DusPsR@cluster0.o40ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+mongoose.connect(mongoUrl,{
+    useNewUrlParser:true,
+})
+.then(()=>{console.log("Connected to Database");})
+.catch((e)=> console.log(e));
+
+require("./userDetails");
+app.use(cors({
+    origin:"*",
+}))
+
+const User = mongoose.model("UserInfo");
+
+app.post('/register',async(req,res)=>{
+
+    const {fname, lname, email, password }=req.body;
+    const encryptedPassword = await bcrypt.hash(password,10);
+    try{
+        const oldUser = await User.findOne({email});
+        if(oldUser){
+           return res.json({error:"User Exist.."});
+        }
+        await User.create({
+            fname,
+            lname,
+            email,
+            password:encryptedPassword,
+        });
+        res.send({status:"ok"});
+    }
+    catch(error){
+        res.send({status:"error..."});
+    }
+});
+
+app.post('/login',async(req,res)=>{
+    const {email, password}=req.body;
+
+    try{
+        const user = await User.findOne({email});
+      if(!user || !(await user.comparePassword(password))){
+          return res.json({status:"Invalid Email/Password"});
+      }
+    }catch(err){
+        res.status(500).json({status:"error"});
+    }
+});
+
+app.listen(80,() => {
+    console.log("Server Started...!");
+});
